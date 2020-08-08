8f250a2761bc10f941322f7de9bc6c08e7578696
Andy <captainrex2000@gmail.com>
2020-08-08 01:25:51 +0000

    Began CardDB Integration

---
+++ CardDB.py
@@ -0,0 +1,11 @@
+import requests
+import json
+import pandas as pd
+import numpy as np
+
+def CardDatabase():
+  YGOProDeck = requests.get('https://db.ygoprodeck.com/api/v7/cardinfo.php')
+  carddb = YGOProDeck.json()['data']
+
+  CardDataFrame = pd.DataFrame(carddb)
+  print(CardDataFrame)
